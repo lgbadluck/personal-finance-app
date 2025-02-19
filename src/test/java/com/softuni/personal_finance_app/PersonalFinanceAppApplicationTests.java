c3402bca9633d95abd8f3b6df9ac601905e3a279
lgbadluck <lg.badluck@gmail.com>
2025-02-19 11:53:29 +0200

    Added properties

---
+++ src/test/java/com/softuni/personal_finance_app/PersonalFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.personal_finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
