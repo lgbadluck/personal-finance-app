438e5ff3b46be620b516306eb3eabc0cbb4e9fa4
lgbadluck <lg.badluck@gmail.com>
2025-02-19 15:37:29 +0200

    Changed template and landing page

--- src/main/java/com/softuni/personal_finance_app/controller/IndexController.java
+++ src/main/java/com/softuni/personal_finance_app/controller/IndexController.java
@@ -1,13 +1,15 @@
 package com.softuni.personal_finance_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+
     @GetMapping("/")
-    public String getIndexPage() {
-        return "index";
+    public String getLandingPage(){
+        return "landing-page";
     }
 }
