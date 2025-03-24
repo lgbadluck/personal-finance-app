9872bf31fdabbe31c0bce9095263e8ebf3ead0ae
lgbadluck <lg.badluck@gmail.com>
2025-03-24 15:23:08 +0200

    Added API Tests for ExceptionAdvice with 100% coverage

---
+++ src/test/java/com/softuni/personal_finance_app/web/ExceptionAdviceApiTest.java
@@ -0,0 +1,104 @@
+package com.softuni.personal_finance_app.web;
+
+import com.softuni.personal_finance_app.enitity.Role;
+import com.softuni.personal_finance_app.exception.DomainException;
+import com.softuni.personal_finance_app.exception.FeignCallException;
+import com.softuni.personal_finance_app.exception.UsernameAlreadyExistException;
+import com.softuni.personal_finance_app.security.AuthenticatedUserDetails;
+import com.softuni.personal_finance_app.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.UUID;
+
+import static com.softuni.personal_finance_app.TestBuilder.aRandomUser;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(IndexController.class)
+public class ExceptionAdviceApiTest {
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void postHandleUsernameAlreadyExist_shouldReturnRegisterViewWithFlashParameter() throws Exception {
+
+        // 1. Build Request
+        MockHttpServletRequestBuilder request = post("/register")
+                .formField("username", "Vik123")
+                .formField("password", "123456")
+                .with(csrf());
+        when(userService.registerUser(any())).thenThrow(new UsernameAlreadyExistException("Username already exist!"));
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/register"))
+                .andExpect(flash().attributeExists("usernameAlreadyExistMessage"));
+    }
+
+
+    @Test
+    void postHandleNotificationFeignCallException_shouldReturnNotificationsViewWithFlashParameter() throws Exception {
+
+        // 1. Build Request
+        MockHttpServletRequestBuilder request = post("/register")
+                .formField("username", "Vik123")
+                .formField("password", "123456")
+                .with(csrf());
+        when(userService.registerUser(any())).thenThrow(new FeignCallException("FeignCallException was called!"));
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/notifications"))
+                .andExpect(flash().attributeExists("clearHistoryErrorMessage"));
+    }
+
+    @Test
+    void postHandleNotFoundExceptions_shouldReturnNotFound() throws Exception {
+
+        // 1. Build Request
+        MockHttpServletRequestBuilder request = post("/register")
+                .formField("username", "Vik123")
+                .formField("password", "123456")
+                .with(csrf());
+        when(userService.registerUser(any())).thenThrow(new AccessDeniedException("AccessDeniedException was called!"));
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("not-found"));
+    }
+
+    @Test
+    void postHandleAnyException_shouldReturnInternalServerError() throws Exception {
+
+        // 1. Build Request
+        MockHttpServletRequestBuilder request = post("/register")
+                .formField("username", "Vik123")
+                .formField("password", "123456")
+                .with(csrf());
+        when(userService.registerUser(any())).thenThrow(new DomainException("DomainException was called!"));
+
+        // 2. Send Request
+        mockMvc.perform(request)
+                .andExpect(status().isInternalServerError())
+                .andExpect(model().attributeExists("errorMessage"))
+                .andExpect(view().name("internal-error"));
+    }
+}
